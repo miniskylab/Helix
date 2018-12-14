3f1119dadaf094bbc6e1687fa21c9309d8467ab3
Cù Đức Hiếu <hieucd04@gmail.com>
2018-12-14 11:33:32 +0700

    Refactored code

---
+++ Deployment/publish.cmd
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+SET entryDirectoryPath="..\Gui"
+SET publishDirectoryPath="%~dp0helix\bin"
+
+IF EXIST %publishDirectoryPath% CALL rmdir /S /Q %publishDirectoryPath%
+CALL mkdir %publishDirectoryPath%
+
+SET mode=[%1]
+IF %mode%==[] SET mode=Release
+CALL dotnet publish %entryDirectoryPath% -c %mode% -r win10-x64 -o %publishDirectoryPath%
+
+CALL robocopy %entryDirectoryPath%\bin\%mode%\netcoreapp2.1\win10-x64 %publishDirectoryPath% chromedriver.exe /NFL /NDL /NJH /NJS /nc /ns /np
+CALL robocopy %entryDirectoryPath%\bin\%mode%\netcoreapp2.1\win10-x64\ui %publishDirectoryPath%\ui /E /NFL /NDL /NJH /NJS /nc /ns /np
+CALL rcedit-x64.exe %publishDirectoryPath%\helix.exe --set-icon icon.ico
+
+PAUSE
