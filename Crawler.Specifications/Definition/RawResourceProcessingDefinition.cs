251d3745b274f8be7fe00e88fc0c690109fd8e1b
Cù Đức Hiếu <hieucd04@gmail.com>
2018-12-24 18:37:26 +0700

    Added a draft of specifications for IResourceProcessor

---
+++ Crawler.Specifications/Definition/RawResourceProcessingDefinition.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Helix.Crawler.Abstractions;
+using Helix.Specifications.Core;
+
+namespace Helix.Crawler.Specifications
+{
+    internal class RawResourceProcessingDefinition : TheoryData<IRawResource, IResource, bool, Type>
+    {
+        public RawResourceProcessingDefinition() { CreateResourceFromRawResource(); }
+
+        void CreateResourceFromRawResource() { }
+    }
+}
--- Crawler.Specifications/Specifications/ResourceProcessor.cs
+++ Crawler.Specifications/Specifications/ResourceProcessor.cs
@@ -1,6 +1,26 @@
+using System;
+using Helix.Crawler.Abstractions;
 using Helix.Specifications;
+using Xunit;
 
 namespace Helix.Crawler.Specifications
 {
-    public class ResourceProcessor : AbstractSpecifications { }
+    public class ResourceProcessor : AbstractSpecifications
+    {
+        [Theory]
+        [ClassData(typeof(RawResourceProcessingDefinition))]
+        void CouldProcessRawResourcesIntoResources(IRawResource rawResource, IResource expectedResource, bool expectedProcessingResult,
+            Type expectedException)
+        {
+            var resourceProcessor = ServiceLocator.Get<IResourceProcessor>();
+            if (expectedException != null)
+                Assert.Throws(expectedException, () => { resourceProcessor.TryProcessRawResource(rawResource, out _); });
+            else
+            {
+                var processingResult = resourceProcessor.TryProcessRawResource(rawResource, out var resource);
+                Assert.Equal(expectedProcessingResult, processingResult);
+                Assert.StrictEqual(expectedResource, resource);
+            }
+        }
+    }
 }
