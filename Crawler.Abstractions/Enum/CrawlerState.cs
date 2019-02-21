68fee33957ba706436b20846d112a26b4cfebeb7
Cù Đức Hiếu <hieucd04@gmail.com>
2019-02-22 00:43:23 +0700

    Refactored code

--- Crawler.Abstractions/Enum/CrawlerState.cs
+++ Crawler.Abstractions/Enum/CrawlerState.cs
@@ -1,13 +1,17 @@
-﻿namespace Helix.Crawler.Abstractions
+﻿using System;
+
+namespace Helix.Crawler.Abstractions
 {
+    [Flags]
     public enum CrawlerState
     {
-        WaitingToRun,
-        Running,
-        Stopping,
-        RanToCompletion,
-        Cancelled,
-        Faulted,
-        Paused
+        WaitingToRun = 1 << 0,
+        Running = 1 << 1,
+        Stopping = 1 << 2,
+        RanToCompletion = 1 << 3,
+        Cancelled = 1 << 4,
+        Faulted = 1 << 5,
+        Paused = 1 << 6,
+        Completed = RanToCompletion | Cancelled | Faulted
     }
 }
