eac72432fd3d0da8b44d9cd115081ca11e1b1bb7
Cù Đức Hiếu <hieucd04@gmail.com>
2019-04-11 18:50:59 +0700

    Added a draft of HardwareMonitor service

---
+++ Crawler/Business/HardwareMonitor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Helix.Crawler
+{
+    public class HardwareMonitor
+    {
+        public async Task<double> GetCpuUsageEvery(TimeSpan timeSpan)
+        {
+            var startTime = DateTime.UtcNow;
+            var startTotalProcessorTime = Process.GetCurrentProcess().TotalProcessorTime; // TODO: all processes
+            await Task.Delay(timeSpan);
+
+            var endTime = DateTime.UtcNow;
+            var endTotalProcessorTime = Process.GetCurrentProcess().TotalProcessorTime;
+            var totalProcessorTime = endTotalProcessorTime - startTotalProcessorTime;
+            var totalElapsedTime = endTime - startTime;
+            return totalProcessorTime / (Environment.ProcessorCount * totalElapsedTime);
+        }
+    }
+}
