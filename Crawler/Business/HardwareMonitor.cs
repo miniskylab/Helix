56516a9a5e443c65d6627ee2c92bed01746d5c9d
Cù Đức Hiếu <hieucd04@gmail.com>
2019-04-11 23:58:52 +0700

    Continued the work on HardwareMonitor service

--- Crawler/Business/HardwareMonitor.cs
+++ Crawler/Business/HardwareMonitor.cs
@@ -1,22 +1,74 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Helix.Crawler
 {
     public class HardwareMonitor
     {
-        public async Task<double> GetCpuUsageEvery(TimeSpan timeSpan)
-        {
-            var startTime = DateTime.UtcNow;
-            var startTotalProcessorTime = Process.GetCurrentProcess().TotalProcessorTime; // TODO: all processes
-            await Task.Delay(timeSpan);
+        CancellationTokenSource _cancellationTokenSource;
+        Task _samplingTask;
 
-            var endTime = DateTime.UtcNow;
-            var endTotalProcessorTime = Process.GetCurrentProcess().TotalProcessorTime;
-            var totalProcessorTime = endTotalProcessorTime - startTotalProcessorTime;
-            var totalElapsedTime = endTime - startTime;
-            return totalProcessorTime / (Environment.ProcessorCount * totalElapsedTime);
+        public event Action OnHighCpuUsage;
+        public event Action OnLowCpuUsage;
+
+        [Obsolete(ErrorMessage.UseDependencyInjection, true)]
+        public HardwareMonitor() { }
+
+        public void Dispose() { StopMonitoring(); }
+
+        public void StartMonitoring(double millisecondSampleInterval = 60000, float highCpuUsageThreshold = 0.8f,
+            float lowCpuUsageThreshold = 0.6f)
+        {
+            if (_samplingTask != null) throw new Exception("Already monitoring.");
+
+            var cpuUtilizationSamples = new List<double>();
+            _cancellationTokenSource = new CancellationTokenSource();
+            _samplingTask = Task.Run(() =>
+            {
+                while (_cancellationTokenSource.IsCancellationRequested)
+                {
+                    var processes = Process.GetProcesses();
+                    var startTime = DateTime.UtcNow;
+                    var startTotalProcessorTime = processes.Aggregate(
+                        TimeSpan.Zero,
+                        (accumulatedTotalProcessorTime, process) => accumulatedTotalProcessorTime + process.TotalProcessorTime
+                    );
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                    var endTime = DateTime.UtcNow;
+                    var endTotalProcessorTime = processes.Aggregate(
+                        TimeSpan.Zero,
+                        (accumulatedTotalProcessorTime, process) => accumulatedTotalProcessorTime + process.TotalProcessorTime
+                    );
+
+                    var totalElapsedTime = endTime - startTime;
+                    var totalProcessorTime = endTotalProcessorTime - startTotalProcessorTime;
+                    cpuUtilizationSamples.Add(totalProcessorTime / (Environment.ProcessorCount * totalElapsedTime));
+                    if (cpuUtilizationSamples.Count < TimeSpan.FromMilliseconds(millisecondSampleInterval).TotalSeconds) continue;
+
+                    CheckCpuUtilization(cpuUtilizationSamples.Average(), highCpuUsageThreshold, lowCpuUsageThreshold);
+                    cpuUtilizationSamples.Clear();
+                }
+            }, _cancellationTokenSource.Token);
+        }
+
+        public void StopMonitoring()
+        {
+            if (_samplingTask == null) throw new Exception("Not started monitoring.");
+            _cancellationTokenSource.Cancel();
+            _samplingTask.Wait();
+
+            _cancellationTokenSource = null;
+            _samplingTask = null;
+        }
+
+        void CheckCpuUtilization(double averageCpuUtilization, float highCpuUsageThreshold, float lowCpuUsageThreshold)
+        {
+            if (averageCpuUtilization >= highCpuUsageThreshold) OnHighCpuUsage?.Invoke();
+            else if (averageCpuUtilization < lowCpuUsageThreshold) OnLowCpuUsage?.Invoke();
         }
     }
 }
