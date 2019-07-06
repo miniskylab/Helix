dad1c9e47fcbc7fa1e2782a77a9a0f8f8e703ddd
Cù Đức Hiếu <hieucd04@gmail.com>
2019-07-07 03:22:31 +0700

    Reduced buffer rate from 2.2 to 1.3

--- Crawler/Business/HardwareMonitor.cs
+++ Crawler/Business/HardwareMonitor.cs
@@ -43,7 +43,7 @@ namespace Helix.Crawler
                         const int millisecondSampleInterval = 1000;
                         Thread.Sleep(millisecondSampleInterval);
 
-                        const float bufferRate = 2.2f; // TODO: to 1.3
+                        const float bufferRate = 1.3f;
                         cpuUsageSamples.Add((int) MathF.Ceiling(performanceCounter.NextValue() * bufferRate));
 
                         var millisecondTotalElapsedTime = cpuUsageSamples.Count * millisecondSampleInterval;
