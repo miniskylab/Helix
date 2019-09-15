31f19f7f400a505d30a79f1d54c169ab1f33d009
Cù Đức Hiếu <hieucd04@gmail.com>
2019-09-16 02:50:41 +0700

    chore: added a draft for CrawlerBotV2

--- Crawler/Application/CrawlerBotV2.cs
+++ Crawler/Application/CrawlerBotV2.cs
@@ -1,7 +1,54 @@
-﻿namespace Helix.Crawler
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Reflection;
+using Helix.Core;
+using Helix.Crawler.Abstractions;
+using log4net;
+
+namespace Helix.Crawler
 {
     public class CrawlerBotV2
     {
-        public CrawlerBotV2() { }
+        readonly ILog _log;
+        readonly StateMachine<CrawlerState, CrawlerCommand> _stateMachine;
+
+        public IStatistics Statistics { get; private set; }
+
+        public CrawlerState CrawlerState => _stateMachine.CurrentState;
+
+        public CrawlerBotV2()
+        {
+            _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+            _stateMachine = new StateMachine<CrawlerState, CrawlerCommand>(
+                new Dictionary<Transition<CrawlerState, CrawlerCommand>, CrawlerState>
+                {
+                    { Transition(CrawlerState.WaitingForInitialization, CrawlerCommand.Stop), CrawlerState.Completed },
+                    { Transition(CrawlerState.WaitingForInitialization, CrawlerCommand.Initialize), CrawlerState.WaitingToRun },
+                    { Transition(CrawlerState.WaitingToRun, CrawlerCommand.Run), CrawlerState.Running },
+                    { Transition(CrawlerState.WaitingToRun, CrawlerCommand.Abort), CrawlerState.WaitingForStop },
+                    { Transition(CrawlerState.WaitingForStop, CrawlerCommand.Stop), CrawlerState.Completed },
+                    { Transition(CrawlerState.Running, CrawlerCommand.Stop), CrawlerState.Completed },
+                    { Transition(CrawlerState.Running, CrawlerCommand.Pause), CrawlerState.Paused },
+                    { Transition(CrawlerState.Completed, CrawlerCommand.MarkAsRanToCompletion), CrawlerState.RanToCompletion },
+                    { Transition(CrawlerState.Completed, CrawlerCommand.MarkAsCancelled), CrawlerState.Cancelled },
+                    { Transition(CrawlerState.Completed, CrawlerCommand.MarkAsFaulted), CrawlerState.Faulted },
+                    { Transition(CrawlerState.Paused, CrawlerCommand.Resume), CrawlerState.Running }
+                },
+                CrawlerState.WaitingForInitialization
+            );
+
+            Transition<CrawlerState, CrawlerCommand> Transition(CrawlerState fromState, CrawlerCommand command)
+            {
+                return new Transition<CrawlerState, CrawlerCommand>(fromState, command);
+            }
+        }
+
+        static CrawlerBotV2()
+        {
+            // TODO: A workaround for .Net Core 2.x bug. Should be removed in the future.
+            AppContext.SetSwitch("System.Net.Http.UseSocketsHttpHandler", false);
+            ServicePointManager.DefaultConnectionLimit = int.MaxValue;
+        }
     }
 }
