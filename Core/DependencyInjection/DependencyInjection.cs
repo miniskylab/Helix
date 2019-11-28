b47c255019264d50c4051027f06ee4036a5f90b6
Cù Đức Hiếu <hieucd04@gmail.com>
2019-11-28 21:14:55 +0700

    chore: reworked assembly scanning mechanism

--- Core/DependencyInjection/DependencyInjection.cs
+++ Core/DependencyInjection/DependencyInjection.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using Autofac;
 
 namespace Helix.Core
@@ -18,8 +20,22 @@ namespace Helix.Core
 
             void RegisterTransientServicesByConvention()
             {
+                var pathToEntryAssembly = Assembly.GetEntryAssembly()?.Location;
+                if (pathToEntryAssembly == null) throw new DllNotFoundException("Entry assembly not found.");
+
+                var workingDirectory = Path.GetDirectoryName(pathToEntryAssembly);
+                if (workingDirectory == null)
+                    throw new DirectoryNotFoundException($"Could not obtain parent directory of: {pathToEntryAssembly}");
+
+                var loadedAssemblies = new List<Assembly>();
+                foreach (var pathToAssemblyFile in Directory.GetFiles(workingDirectory, "*.dll", SearchOption.AllDirectories))
+                {
+                    try { loadedAssemblies.Add(Assembly.LoadFrom(pathToAssemblyFile)); }
+                    catch (FileLoadException) { }
+                    catch (BadImageFormatException) { }
+                }
+
                 var registrableTypes = new List<Type>();
-                var loadedAssemblies = AppDomain.CurrentDomain.GetAssemblies();
                 var registrable = new Func<Type, bool>(
                     type => type.IsClass &&
                             type.IsAssignableTo<IService>() &&
