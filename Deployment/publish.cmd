5cb038d2fe0bfbd49dfa4ed3f3f0dca6abe5b9ae
Cù Đức Hiếu <hieucd04@gmail.com>
2019-12-25 17:38:00 +0700

    chore: upgraded to .NET Core 3.1

--- Deployment/publish.cmd
+++ Deployment/publish.cmd
@@ -10,10 +10,10 @@ SET mode=[%1]
 IF %mode%==[] SET mode=Release
 CALL dotnet publish %entryDirectoryPath% -c %mode% -r win10-x64 -o %publishDirectoryPath%
 
-CALL robocopy %entryDirectoryPath%\bin\%mode%\netcoreapp3.0\win10-x64\ui %publishDirectoryPath%\ui /E /NFL /NDL /NJH /NJS /nc /ns /np
-CALL robocopy %entryDirectoryPath%\bin\%mode%\netcoreapp3.0\win10-x64 %publishDirectoryPath% chromedriver.exe /NFL /NDL /NJH /NJS /nc /ns /np
-CALL robocopy %entryDirectoryPath%\bin\%mode%\netcoreapp3.0\win10-x64\chromium %publishDirectoryPath%\chromium /E /NFL /NDL /NJH /NJS /nc /ns /np
-CALL robocopy %entryDirectoryPath%\bin\%mode%\netcoreapp3.0\win10-x64\sqlite-browser %publishDirectoryPath%\sqlite-browser /E /NFL /NDL /NJH /NJS /nc /ns /np
+CALL robocopy %entryDirectoryPath%\bin\%mode%\netcoreapp3.1\win10-x64\ui %publishDirectoryPath%\ui /E /NFL /NDL /NJH /NJS /nc /ns /np
+CALL robocopy %entryDirectoryPath%\bin\%mode%\netcoreapp3.1\win10-x64 %publishDirectoryPath% chromedriver.exe /NFL /NDL /NJH /NJS /nc /ns /np
+CALL robocopy %entryDirectoryPath%\bin\%mode%\netcoreapp3.1\win10-x64\chromium %publishDirectoryPath%\chromium /E /NFL /NDL /NJH /NJS /nc /ns /np
+CALL robocopy %entryDirectoryPath%\bin\%mode%\netcoreapp3.1\win10-x64\sqlite-browser %publishDirectoryPath%\sqlite-browser /E /NFL /NDL /NJH /NJS /nc /ns /np
 CALL rcedit-x64.exe %publishDirectoryPath%\helix.exe --set-icon icon.ico
 
 PAUSE
